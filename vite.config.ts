c1ba51462716d427301fd142b63c02086867e0de
KSundance <kaushik.thallapally@da2softwaresolutions.com>
2026-05-14 19:31:42 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  base: '/Samastham-Website/',
+  base: '/',
   plugins: [
     react(),
     tailwindcss(),
